54086ae0d282246326ddaf15dfe0fe48d5d3ee59
JJRedy <jose.claveria.p@gmail.com>
2025-03-24 20:53:43 -0300

    Añadi pagina de login

--- sever.ts
+++ sever.ts
@@ -3,17 +3,16 @@ import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
-// Define rutas básicas
-router
-  .get("/", async (context) => {
-    await send(context, "index.html", {
-      root: `${Deno.cwd()}`,
-    });
-  });
+router.get("/", async (ctx) => {
+    await send(ctx, "index.html", { root: `${Deno.cwd()}/public` });
+});
+
+router.get("/login", async (ctx) => {
+    await send(ctx, "login.html", { root: `${Deno.cwd()}/public` });
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// Inicia el servidor
 console.log("Servidor corriendo en http://localhost:8000");
 await app.listen({ port: 8000 });
