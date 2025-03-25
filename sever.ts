409ec5beb7ba0bdc1e59bb78408ff782f3607408
ManuelV2 <ma.vergara@alumnos.uai.cl>
2025-03-24 21:17:57 -0300

    Navbar

--- sever.ts
+++ sever.ts
@@ -3,6 +3,18 @@ import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
+// Servir archivos estáticos
+router.get("/components/:file", async (ctx) => {
+    const file = ctx.params.file;
+    await send(ctx, `components/${file}`, { root: `${Deno.cwd()}/public` });
+});
+
+router.get("/js/:file", async (ctx) => {
+    const file = ctx.params.file;
+    await send(ctx, `js/${file}`, { root: `${Deno.cwd()}/public` });
+});
+
+// Rutas existentes
 router.get("/", async (ctx) => {
     await send(ctx, "index.html", { root: `${Deno.cwd()}/public` });
 });
